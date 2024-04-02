cfc69cc79995fd854bfe0c5935b93ad3ec62bf11
Richard <Richardju@techtrex.com>
2024-04-02 11:05:05 +0800

    feature：控制台日志添加颜色

--- log/logger.go
+++ log/logger.go
@@ -7,6 +7,24 @@ import (
 	"time"
 )
 
+const (
+	greenBg   = "\033[97;42m"
+	whiteBg   = "\033[90;47m"
+	yellowBg  = "\033[90;43m"
+	redBg     = "\033[97;41m"
+	blueBg    = "\033[97;44m"
+	magentaBg = "\033[97;45m"
+	cyanBg    = "\033[97;46m"
+	green     = "\033[32m"
+	white     = "\033[37m"
+	yellow    = "\033[33m"
+	red       = "\033[31m"
+	blue      = "\033[34m"
+	magenta   = "\033[35m"
+	cyan      = "\033[36m"
+	reset     = "\033[0m"
+)
+
 type LoggerLevel int
 
 const (
@@ -22,7 +40,8 @@ type Logger struct {
 }
 
 type LoggerFormatter struct {
-	Level LoggerLevel
+	Level   LoggerLevel
+	IsColor bool
 }
 
 func DefaultLogger() *Logger {
@@ -58,6 +77,10 @@ func (l *Logger) Print(level LoggerLevel, args any) {
 	l.Formatter.Level = level
 	formatter := l.Formatter.formatter(args)
 	for _, out := range l.Outs {
+		if out == os.Stdout {
+			l.Formatter.IsColor = true
+			formatter = l.Formatter.formatter(args)
+		}
 		fmt.Fprint(out, formatter)
 	}
 
@@ -65,10 +88,44 @@ func (l *Logger) Print(level LoggerLevel, args any) {
 
 func (f *LoggerFormatter) formatter(msg any) string {
 	now := time.Now()
+	if f.IsColor {
+		levelColor := f.LevelColor()
+		msgColor := f.MsgColor()
+		return fmt.Sprintf("%s [msgo] %s %s%v%s | level= %s %s %s | msg=%s %#v %s \n",
+			yellow, reset, blue, now.Format("2006/01/02 - 15:04:05"), reset,
+			levelColor, f.Level.Level(), reset, msgColor, msg, reset,
+		)
+	}
 	return fmt.Sprintf("[go-framework] %v | level=%s | msg=%#v \n",
 		now.Format("2006/01/02 - 15:04:05"),
 		f.Level.Level(), msg,
 	)
+}
+
+func (f *LoggerFormatter) LevelColor() string {
+	switch f.Level {
+	case LevelDebug:
+		return blue
+	case LevelInfo:
+		return green
+	case LevelError:
+		return red
+	default:
+		return cyan
+	}
+}
+
+func (f *LoggerFormatter) MsgColor() string {
+	switch f.Level {
+	case LevelDebug:
+		return ""
+	case LevelInfo:
+		return ""
+	case LevelError:
+		return red
+	default:
+		return cyan
+	}
 }
 
 func (level LoggerLevel) Level() string {
